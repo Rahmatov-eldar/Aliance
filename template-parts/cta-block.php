e52fdc7ba55654fd83de8dc3cd961a5bf4f23fb4
Rahmatov-eldar <Rahmatov-eldar291207@gmail.com>
2024-07-10 18:07:24 +0800

    all

---
+++ template-parts/cta-block.php
@@ -0,0 +1,33 @@
+<section class="cta">
+    <div class="bg-grey section-gta">
+    <picture>
+          <source type="image/webp" srcset="img/cta.webp"> 
+          <source type="image/jpeg" srcset="img/cta.png"> 
+          <img src="img/cta.png" alt="" class="cta-image" />
+        </picture>
+      <div class="cta-form-wrapper container">
+        <form action="handler.php" method="POST" class="cta-form">
+         <div class="seporator">
+          <h2 class="sectiong-title cta-form-title">Генеральный дистрибьютор премиальной автохимии</h2>
+          </div>
+          <p class="gta-form-text">Внезапно, активно развивающиеся страны третьего мира могут быть объявлены нарушающими общечеловеческие нормы этики и морали.</p>
+        </form>
+        <ul class="gta-list">
+        <li class="clients-list-item"><svg width="30" height="30" class="clients-list-icon">
+          <use href="/img/sprite.svg#him"></use>
+        </svg>Полное описание товаров</li>
+          <li class="clients-list-item"><svg width="30" height="30" class="clients-list-icon">
+          <use href="/img/sprite.svg#car"></use>
+        </svg>Актуальные цены</li>
+          <li class="clients-list-item"><svg width="30" height="30" class="clients-list-icon">
+          <use href="/img/sprite.svg#him"></use>
+        </svg>Полное описание товаров</li>
+          <li class="clients-list-item"><svg width="30" height="30" class="clients-list-icon">
+          <use href="/img/sprite.svg#car"></use>
+        </svg>Актуальные цены</li>
+        </ul>
+        <button class="button header-button" data-toggle="modal" data-target="#feedback-modal">
+          Скачать каталог
+        </button>
+    </div>
+  </section>
